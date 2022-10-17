dbc29467869d7b16e259be64b932381b9b6d5bcd
Bassel Al Omari <bassel.al.omari@autodesk.com>
2022-10-17 03:13:11 -0400

    Created TextDataset class

--- client/clientDataset.py
+++ client/clientDataset.py
@@ -1,6 +1,29 @@
 import collections
 import random
-import torch
+
+from torch.utils.data import Dataset, DataLoader, random_split
+
+
+class TextDataset(Dataset):
+    def __init__(self, text, vocab, batchSize):
+        self.tokenizedDataset = [vocab[c] for c in text]
+        self.batchSize = batchSize
+
+    def __getitem__(self, index):
+        low = index * self.batchSize
+        high = (index + 1) * self.batchSize
+        x = self.tokenizedDataset[low:high]
+        y = None
+        try:
+            y = self.tokenizedDataset[low + 1 : high + 1]
+        except IndexError:
+            y = x
+            x = self.tokenizedDataset[low - 1 : high - 1]
+        return x, y
+
+    # Number of batches
+    def __len__(self):
+        return len(self.tokenizedDataset) // self.batchSize
 
 
 def get_user_data(userCount, userID, rounds, seed):
@@ -11,7 +34,6 @@ def get_user_data(userCount, userID, rounds, seed):
         text = f.read()
 
     splitParagraphs = text.split("\n\n")
-    print(len(splitParagraphs))
     random.shuffle(splitParagraphs)
     numParagraphsPerUser = len(splitParagraphs) / userCount
     numParagraphsPerRound = numParagraphsPerUser / rounds
@@ -22,7 +44,6 @@ def get_user_data(userCount, userID, rounds, seed):
     for i in range(rounds):
         low = int(start + (numParagraphsPerRound * i))
         high = int(start + (numParagraphsPerRound * (i + 1)))
-        print(low, high)
         round_split_data.append("\n\n".join(splitParagraphs[low:high]))
 
     return round_split_data
