9ef0d4ad7a47860f366341107606c0d452d2a4e2
Bassel Al Omari <bassel.al.omari@autodesk.com>
2022-10-24 00:11:42 -0400

    feat: designed neural network

---
+++ model/layers.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+
+class CharRNN(nn.Module):
+    def __init__(self, inputSize, hiddenSize, outputSize, nLayers=2):
+        super(CharRNN, self).__init__()
+        self.inputSize = inputSize
+        self.hiddenSize = hiddenSize
+        self.outputSize = outputSize
+        self.nLayers = nLayers
+
+        self.encoder = nn.Embedding(inputSize, hiddenSize)
+        self.rnn = nn.LSTM(hiddenSize, hiddenSize, nLayers)
+        self.decoder = nn.Linear(hiddenSize, outputSize)
+
+    def forward(self, input, hidden):
+        encoded = self.encoder(input)
+        output, hidden = self.rnn(encoded, hidden)
+        output = self.decoder(output)
+        return output, hidden
+
+    def init_hidden(self):
+        return (
+            torch.zeros(self.nLayers, self.hiddenSize),
+            torch.zeros(self.nLayers, self.hiddenSize),
+        )
