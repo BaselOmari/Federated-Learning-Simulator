ebf465aae0804b4db8a037f5ba315bd0b6ac2ebd
Bassel Al Omari <bassel.al.omari@autodesk.com>
2022-10-25 00:47:03 -0400

    feat: added generating and sampling

--- model/train.py
+++ model/train.py
@@ -1,11 +1,13 @@
 import torch
 import torch.nn as nn
 from torch import optim
+import torch.nn.functional as F
 from torch.utils.data import random_split
+from torch.distributions import Categorical
 from math import ceil, floor
 
 
-def train(model, dataset):
+def train(model, dataset, reverseVocab, vocab):
 
     ##### TRAINING HYPERPARAMETERS #####
     epochs = 24
@@ -37,5 +39,65 @@ def train(model, dataset):
 
         if epoch % 2 == 0:
             print(f"EPOCH {epoch} LOSS: {epochLoss}")
+            print(generate(model, "And she shall than", 100, reverseVocab, vocab))
 
     return model
+
+
+def sample(output):
+    output = F.softmax(torch.squeeze(output), dim=0)
+    dist = Categorical(output)
+    index = dist.sample()
+    return index.item()
+
+
+def generate(model, startStr, length, reverseVocab, vocab):
+    encodedStr = torch.tensor([reverseVocab[c] for c in startStr])
+    finalStr = startStr
+
+    # take in starting string
+    _, hidden = model(encodedStr)
+
+    # generate next characters
+    prevChar = encodedStr[-1]
+    for i in range(length):
+        prevChar = prevChar.reshape([1])
+        output, hidden = model(prevChar, hidden)
+        generatedCharNum = sample(output)
+        generatedChar = vocab[generatedCharNum]
+        finalStr += generatedChar
+
+        prevChar = torch.tensor(generatedCharNum)
+
+    return finalStr
+
+
+import sys
+
+sys.path.append(r"/Users/alomarb/non-adsk/FL Research/FL-Sim")
+
+
+from layers import CharRNN
+from client import clientDataset
+
+rounds = 4
+
+roundSplitDataset = clientDataset.get_user_data(5, 0, rounds, 20202)
+vocab, reverseVocab = clientDataset.get_vocab()
+
+for i in range(rounds):
+    currRoundText = roundSplitDataset[i]
+    currRoundDataset = clientDataset.TextDataset(currRoundText, reverseVocab, 100)
+
+    trainSet, testSet = random_split(
+        currRoundDataset,
+        [ceil(len(currRoundDataset) * 0.8), floor(len(currRoundDataset) * 0.2)],
+    )
+
+    freshModel = CharRNN(
+        len(reverseVocab),
+        16,
+        len(reverseVocab),
+        nLayers=2,
+    )
+    trainedModel = train(freshModel, trainSet, reverseVocab, vocab)
