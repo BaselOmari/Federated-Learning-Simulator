159ea3d6360982e217020a2706bc1226d2fcc917
Bassel Al Omari <bassel.al.omari@autodesk.com>
2022-10-30 15:50:01 -0400

    feat: added new mnist dataset and dataloader

--- client/clientDataset.py
+++ client/clientDataset.py
@@ -1,4 +1,38 @@
-import torch
-import torchvision
+import os
 
+import torchvision
+from torch.utils.data import DataLoader, Dataset, Subset
 from torchvision import datasets
+
+
+def load_full_dataset() -> Dataset:
+    mnistDataset = datasets.MNIST(
+        os.path.dirname(os.path.realpath(__file__)) + "/data",
+        train=True,
+        download=True,
+        transform=torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        ),
+    )
+    return mnistDataset
+
+
+def create_data_subset(count, index, fullDataset=load_full_dataset()) -> Subset:
+    countPerSet = len(fullDataset) // count
+    low = countPerSet * index
+    high = low + countPerSet
+    subsetIndices = [i for i in range(low, high)]
+    return Subset(fullDataset, subsetIndices)
+
+
+def get_round_dataloader(roundNumber, roundCount, batchSize, userDataset) -> DataLoader:
+    subset = create_data_subset(roundCount, roundNumber, userDataset)
+    dataloader = DataLoader(
+        subset,
+        batch_size=batchSize,
+        shuffle=True,
+    )
+    return dataloader
