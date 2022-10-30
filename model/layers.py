9550fc39a1216ccee18c544f8376448635d8bbcb
Bassel Al Omari <bassel.al.omari@autodesk.com>
2022-10-30 11:49:42 -0400

    fix: change layers to match MNIST dataset

--- model/layers.py
+++ model/layers.py
@@ -1,21 +1,21 @@
-import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
-class CharRNN(nn.Module):
-    def __init__(self, inputSize, hiddenSize, outputSize, nLayers=2):
-        super(CharRNN, self).__init__()
-        self.inputSize = inputSize
-        self.hiddenSize = hiddenSize
-        self.outputSize = outputSize
-        self.nLayers = nLayers
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
 
-        self.encoder = nn.Embedding(inputSize, hiddenSize)
-        self.rnn = nn.LSTM(hiddenSize, hiddenSize, nLayers)
-        self.decoder = nn.Linear(hiddenSize, outputSize)
-
-    def forward(self, input, hidden=None):
-        encoded = self.encoder(input)
-        output, hidden = self.rnn(encoded, hidden)
-        output = self.decoder(output)
-        return output, hidden
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
