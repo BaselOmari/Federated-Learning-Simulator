d3ce922005dcfb119438a9118b01a853a6e5a4c2
Bassel Al Omari <b2alomar@uwaterloo.ca>
2023-01-11 22:01:19 +0800

    feat: implemented sequential federated learning

--- federated.py
+++ federated.py
@@ -2,8 +2,10 @@ import torch
 
 from copy import deepcopy
 
+from dataset import mnist_dataset
+
 from model.layers import CNN
-from model.train import train
+from model.train import train, test
 
 def fedAvg(clientModels):
     averagedModel = deepcopy(clientModels[0])
@@ -22,8 +24,27 @@ class federatedConfig:
 
 
 def federated():
-    pass
+    config = federatedConfig()
 
+    trainSet = mnist_dataset.load_dataset(isTrainDataset=True)
+    trainLoader = mnist_dataset.get_dataloader(trainSet)
+
+    testSet = mnist_dataset.load_dataset(isTrainDataset=False)
+    testLoader = mnist_dataset.get_dataloader(testSet)
+
+    serverModel = CNN()
+
+    for round in range(config.trainingRounds):
+        print(f"Round {round} started")
+        clientModels = []
+        for client in range(config.clientNum):
+            clientModel = deepcopy(serverModel)
+            train(clientModel, trainLoader)
+            clientModels.append(clientModel)
+            print(f"Client {client} done")
+        serverModel = fedAvg(clientModels)
+        testAcc = test(serverModel, testLoader)
+        print(f"Round {round} done\tAccuracy = {testAcc}\n\n")
 
 
 if __name__ == "__main__":
